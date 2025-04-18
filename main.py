0365e580d345fd8a1dc928ff192cf6a1e97fda2e
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-04-18 21:14:20 +0200

    Very simple main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from board import *
-side_to_move = "X"
-board = create_board()
-render(board)
 
-board = make_move(get_move(),side_to_move,board)
-render(board)
+#This should track the move, even for O, odds for X
+move = 1
+
+board = create_board()
+
+while(game_check(board) == False):
+    render(board)
+    board = make_move(get_move(),side_to_move(move),board)
+    move +=1
