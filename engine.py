a3e497073c67d3fed783e85a9221fd7a0beef536
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-04-19 22:41:56 +0200

    Preping for the whole computer playing shabang, no clue how to do it atm

---
+++ engine.py
@@ -0,0 +1,14 @@
+from board import element_list
+#This is where our computer opponent should decide a move that it will make
+#It should recive a board and return a move
+
+
+
+def return_ai_move(board):
+    #This should return CORDINATES of the move
+    pass
+
+
+def calculate_move(board):
+    #Here we need to add some smart stuff that will allow the ai to pick moves (good ones preferably)
+    pass
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from board import *
-
+from engine import *
 #This should track the move, even for O, odds for X
 move = 1
 
@@ -7,5 +7,7 @@ board = create_board()
 
 while(game_check(board) == False):
     render(board)
-    board = make_move(get_move(),side_to_move(move),board)
-    move +=1
+    if side_to_move(move) == "X":
+        board = make_move(get_move(),"X",board)
+    elif side_to_move(move) == "O":
+        board = make_move(return_ai_move(board),"O",board)
